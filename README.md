dcd1cd5bf7d5af112b357150c0e99a04f1df0c0d
Son <son.caokim@gmail.com>
2024-06-14 22:03:49 +0200

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # hash-bang
+
+Turn any program source code into an executable, by adding hash-bang line on the top
+
+The dependency and build process is taken care of.
+
+Minimum requirement: `cut`, `dirname`, `grep`, `md5sum`, `rm`, `sed`, `touch`, `zsh`,
+and Unix-based system (so that `#!` magic numbers are managed)
+
+Insprired by https://github.com/igor-petruk/scriptisto
+
+See files in `tests/samples-*` for samples
--- src/main.zsh
+++ src/main.zsh
@@ -1,15 +1,5 @@
 #!/usr/bin/env zsh
 
-# Turn any program source code into an executable, by adding hash-bang line on the top
-# The dependency and build process is taken care of
-#
-# Minimum requirement: cut, dirname, grep, md5sum, rm, sed, touch, zsh 
-# and Unix-based system (so that `#!` magic numbers are managed)
-#
-# Insprired by https://github.com/igor-petruk/scriptisto
-#
-# See files in `tests/samples-*` for samples
-
 # TODO Move this to `src/common.zsh`
 NAME="hash-bang"
 
