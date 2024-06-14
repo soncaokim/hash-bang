c9ad54332c031fef71075d47f09c5d0f2e44eba0
Son <son.caokim@gmail.com>
2024-06-14 22:01:05 +0200

    Rename demo-* sample-*

--- src/main.zsh
+++ src/main.zsh
@@ -2,11 +2,13 @@
 
 # Turn any program source code into an executable, by adding hash-bang line on the top
 # The dependency and build process is taken care of
-# Minimum requirement: cut, grep, md5sum, sed, zsh and Unix-based system (so that `#!` magic numbers are managed)
+#
+# Minimum requirement: cut, dirname, grep, md5sum, rm, sed, touch, zsh 
+# and Unix-based system (so that `#!` magic numbers are managed)
 #
 # Insprired by https://github.com/igor-petruk/scriptisto
 #
-# See files in `tests/demo-*` for examples and demo
+# See files in `tests/samples-*` for samples
 
 # TODO Move this to `src/common.zsh`
 NAME="hash-bang"
