bd7944af90fb10954f4997d55c584baa58489dbc
Son <son.caokim@gmail.com>
2024-07-02 20:04:58 +0200

    python3 by default

---
+++ tests/sample-python.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env -S hash-bang python
+
+# hash-bang:setup: python3 -m pip install termcolor
+
+import termcolor
+print(termcolor.colored("Hey, this is a demo from Python", "green"))
--- tests/sample-python3.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env -S hash-bang python3
-
-# hash-bang:setup: python3 -m pip install termcolor
-
-import termcolor
-print(termcolor.colored("Hey, this is a demo from Python3", "green"))
