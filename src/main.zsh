841b609e69d526773e244e6bb5302d81273e0d62
Son <son.caokim@gmail.com>
2024-09-08 15:05:59 +0200

    Add logging of hashbang executions to a .csv file

--- src/main.zsh
+++ src/main.zsh
@@ -5,6 +5,7 @@ NAME="hash-bang"
 
 BASE_PATH=${0:A:h:h} # ':A' gives the pathname, ':h' gives the parent
 CACHE_PATH="${HOME}/.cache/${NAME}"
+LOG_FILE="${NAME}_log.csv"
 
 source "${BASE_PATH}/src/lib/logging"
 
